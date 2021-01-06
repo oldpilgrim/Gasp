302787106b4c51e328374688e6a75ea1b8f33597
Fred Chen <fredchen1994@outlook.com>
2021-01-06 17:13:20 +0000

    finish Congo

---
+++ Congo/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace Congo.Models
+{
+    public class Book
+    {
+        // designate this property as the document's primary key.
+        [BsonId]
+        // allow passing the parameter as type string instead of an ObjectId structure.
+        // Mongo handles the conversion from string to ObjectId.
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        // Name represents the property name in the MongoDB collection.
+        [BsonElement("Name")]
+        // Serialised as Name in JSON, and not BookName (member casing) or bookName (default)
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
--- Congo/Startup.cs
+++ Congo/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Congo.Models;
+using Congo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace Congo
@@ -26,7 +29,26 @@ namespace Congo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            // The configuration instance to which the appsettings.json file's BookstoreDatabaseSettings section binds
+            // is registered in the Dependency Injection (DI) container.
+            services.Configure<BookstoreDatabaseSettings>(
+                Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+            
+            // The IBookstoreDatabaseSettings interface is registered in DI with a singleton service lifetime.
+            // When injected, the interface instance resolves to a BookstoreDatabaseSettings object.
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+            
+            // the BookService class is registered with DI to support constructor injection in consuming classes.
+            // The singleton service lifetime is most appropriate because BookService takes a direct dependency
+            // on MongoClient. Per the official Mongo Client reuse guidelines,
+            // MongoClient should be registered in DI with a singleton service lifetime.
+            services.AddSingleton<BookService>();
+
+            // Make property names in the web API's serialized JSON response match their corresponding property names in the CLR object type
+            // For example, the Book class's Author property serializes as Author.
+            services.AddControllers().AddNewtonsoftJson(options => options.UseMemberCasing());
+            
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Congo", Version = "v1"}); });
         }
 
